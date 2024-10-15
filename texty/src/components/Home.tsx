c83a411cc37e63b76176ae5de29e7e4bc0c6ab76
Simpl1fy <gourabdas07012004@gmail.com>
2024-10-15 11:24:11 +0530

    added props for contact selected for logical textarea view

--- texty/src/components/Home.tsx
+++ texty/src/components/Home.tsx
@@ -1,10 +1,20 @@
+import { useState } from "react"
 import Sidebar from "./Sidebar"
 import TextPage from "./TextPage"
 
 export default function Home() {
+
+  /* 
+    Creating a state for handling contact click to show the text page
+    or a default page which includes (select a contact to start typing)
+  */
+  const [selectedContact, setSelectedContact] = useState<{name: string; id: number}>({name: "",
+    id: 0
+  })
+
   return (
     <div className="flex flex-1 min-h-[calc(100vh-6rem)] space-x-1 shadow-md">
-        <Sidebar />
+        <Sidebar selectedContact={selectedContact} setSelectedContact={setSelectedContact} />
         <TextPage />
     </div>
   )
--- texty/src/components/Sidebar.tsx
+++ texty/src/components/Sidebar.tsx
@@ -1,13 +1,33 @@
 import AddCircleIcon from '@mui/icons-material/AddCircle';
-import ContactSearchBar from './ContactSearchBar';
+import SearchIcon from '@mui/icons-material/Search';
+import React from 'react';
 
-export default function Sidebar() {
+interface SidebarProps {
+  selectedContact: {
+    name: string;
+    id: number
+  };
+  setSelectedContact: React.Dispatch<React.SetStateAction<{name: string; id: number}>>
+}
+
+export default function Sidebar({ selectedContact, setSelectedContact }: SidebarProps) {
   return (
     <>
       <div className="w-1/4 bg-gray-200 mt-2 relative">
+
+        {/* Search Bar at the top of the contact list */}
         <nav className="py-3.5 px-4 bg-white shadow-md">
-          <ContactSearchBar />
+          <div className="flex  items-center rounded-lg border-2 border-zinc-600 bg-sky-50">
+            <div className='px-2'><SearchIcon/></div>
+            <input type="text" placeholder="Search in your contacts" className="w-full h-full py-2 px-3 outline-none rounded-r-lg" />
+          </div>
         </nav>
+        
+        {/* Currently keeping this a button */}
+        <button className='p-4 bg-red-500 border-2 rounded-xl' onClick={() => {setSelectedContact({name: "New Contact", id: 1}); console.log(selectedContact)}}>
+          Change Contact
+        </button>
+        
         <div className='absolute bottom-4 right-2 hover:cursor-pointer'>
           <AddCircleIcon fontSize='large' />
         </div>
